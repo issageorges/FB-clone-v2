fae6a7820943df80772415d864ff08b9cadc1b3e
issa georges <issageorges.qi@gmail.com>
2024-01-30 12:06:08 +0100

    fixed

--- client/src/components/userImg.jsx
+++ client/src/components/userImg.jsx
@@ -4,6 +4,11 @@ import { baseUrl } from "../config/api";
 export default function UserImg() {
     const { user } = useUserContext();
 
+    if (!user) {
+        
+        return null;
+    }
+
     return (
         <>
          <img src={baseUrl + "/" + user.profileImg} alt="" name="profile/image" className="rounded-full h-8 w-8 object-cover"/>
